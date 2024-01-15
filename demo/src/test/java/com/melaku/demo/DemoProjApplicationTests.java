f73af0a10adeca9935847fd1e8592e2eafa44dc6
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2024-01-15 12:41:09 +0100

    initial commit
    2 test projs for spring boot

---
+++ demo/src/test/java/com/melaku/demo/DemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melaku.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
